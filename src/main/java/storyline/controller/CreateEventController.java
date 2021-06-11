9284941b32aff5af66be9936b1bae5b82eaccff9
LaPunk-Archive <61421928+LaPunk-Archive@users.noreply.github.com>
2021-06-11 14:07:10 +0200

    create event ui stuff

---
+++ src/main/java/storyline/controller/CreateEventController.java
@@ -0,0 +1,56 @@
+package storyline.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import storyline.model.EventCard;
+
+import java.io.IOException;
+
+public class CreateEventController {
+    @FXML
+    Button btnAccept;
+
+    @FXML
+    TextField fldTitle;
+
+    @FXML
+    TextArea areaContent;
+
+    @FXML
+    ChoiceBox<String> choiceColor;
+
+    @FXML
+    private void initialize() {
+        choiceColor.getItems().addAll("Red", "Blue", "Green");
+    }
+
+    @FXML
+    public void handleAccept() {
+        EventCard eventCard = new EventCard(fldTitle.getText(), getColor(choiceColor.getSelectionModel().getSelectedItem()),
+                areaContent.getText());
+        try {
+            Context.getInstance().projectPageController.cardsEntitiesController.createCard(eventCard);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public String getColor(String choice) {
+        switch (choice) {
+
+            case "Red":
+                return CardsEntitiesController.RED.toString();
+            case "Blue":
+                return CardsEntitiesController.GREEN.toString();
+            case "Green":
+                return CardsEntitiesController.BLUE.toString();
+        }
+        return null;
+    }
+}
